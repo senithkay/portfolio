bf245f28777a57be237ef628741454f35fee24fd
Senith Uthsara <senithkarunarathneu@gmail.com>
2023-05-17 20:58:32 +0530

    scrollable description box

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
 
+import { useState , useRef, useEffect } from 'react';
 import './App.css';
 import DescriptionBox from './components/descriptionBox';
 
+
 function App() {
+
+  const descriptionRef = useRef(null);
+
+    useEffect(()=>{
+        const handleScroll = () =>{
+            const viewHeight = descriptionRef.current.viewHeight;
+            descriptionRef.current.scrollTop += viewHeight
+        }
+
+        window.addEventListener('wheel', handleScroll);
+
+
+        return () => {
+            window.removeEventListener('wheel', handleScroll);
+          };
+    }, [])
+
+  const [count] = useState(2);
+
+  
+  const renderDescriptionBoxes = () => {
+    const descriptionBoxes = [];
+    for (let i = 0 ; i < count; i++){
+      descriptionBoxes.push(<DescriptionBox key={i}/>)
+    }
+    return descriptionBoxes;
+  }
+
   return (
-    <div className="App bg-portbg h-screen w-screen flex justify-center items-center">
-    <DescriptionBox/>
+    <div className="App bg-portbg h-screen w-screen flex justify-center items-center flex-col overflow-hidden">
+
+      <div ref={descriptionRef} style={{ scrollbarWidth: 'none', '-ms-overflow-style': 'none' }} className=' scrollbar-hide w-700 h-96 overflow-hidden overflow-y-scroll'>
+        {renderDescriptionBoxes()}
+      </div>
     </div>
   );
 }
--- src/components/descriptionBox.js
+++ src/components/descriptionBox.js
@@ -1,18 +1,56 @@
-import React from 'react';
+import React, {useState } from 'react';
 import descriptionImage from '../images/OIP.jpg'
+import { Tilt } from 'react-tilt';
 
-const descriptionBox = () => {
-  return  <div className=' flex relative justify-center items-center'>
-        <img src={descriptionImage} alt='description' className=' w-600 h-96 opacity-40'></img>
-        <div className=' w-96 h-80 flex flex-col justify-center text-left absolute left-450 '>
-            <h2 className=' text-portTextPink text-7xl'>Where I Studied:</h2>
-            <h2 className=' text-portTextLightPink text-7xl'>UCSC</h2>
-            <p className=' text-portTextLightPink'>
-                I am an Computer science undergraduate from University of Colombo School of Computing. Currently I am in the third year.
-            </p>
+
+const DescriptionBox = (props) => {
+
+    
+
+    const [isHovered, setIsHovered] = useState(false);
+
+    const handleHover = ()=>{
+        setIsHovered(!isHovered);
+    }
+
+    const hoveredStyle = {
+        transform : 'translateX(-50%)'
+
+    }
+
+    const hoveredImageStyle = {
+        filter: 'blur(8px)',
+    }
+
+    
+
+    const notHoveredStyle = {
+        transform : 'translateX(100%)'
+    }
+
+    const notHoveredImageStyle = {
+        filter: 'blur(0px)',
+    }
+
+
+  return (
+    <Tilt options={{max : 5, scale : 1.0}}>
+         <div onMouseEnter={handleHover} onMouseLeave={handleHover}  className=' flex relative justify-center items-center overflow-hidden'>
+            <img style={isHovered ? hoveredImageStyle : notHoveredImageStyle } src={descriptionImage} alt='description' className=' w-600 h-96 
+            opacity-40 transition-all'></img>
+            <div style={isHovered ? hoveredStyle : notHoveredStyle } className=' w-96 h-80 flex flex-col justify-center 
+            text-left absolute left-450 transition-all'>
+                <h2 className=' text-portTextPink text-7xl'>Where I Studied:</h2>
+                <h2 className=' text-portTextLightPink text-7xl'>UCSC</h2>
+                <p className=' text-portTextLightPink'>
+                    I am an Computer science undergraduate from University of Colombo School of Computing. Currently I am in the third year.
+                </p>
+            </div>
         </div>
-    </div>
+    </Tilt>
+
+  )
     ;
 };
 
-export default descriptionBox;
+export default DescriptionBox;
